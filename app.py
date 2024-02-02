4f4d7e09336c1e70d63a5a48228a3514926fa66f
HARSHharsh123 <hs0814497@gmail.com>
2024-02-02 17:10:12 +0530

    data Transformation Done

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
-
+from src.mlproject.components.data_transformation import DataTransformationConfig , DataTransformation
 import sys
 if __name__ == "__main__":
     logging.info("Logging has Successfully Started")
 
     try:
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path , test_data_path = data_ingestion.initiate_data_ingestion()
+
+        data_tranformation = DataTransformation()
+        data_tranformation.initiate_data_transormation(train_data_path , test_data_path)
+
 
     except Exception as e:
         logging.info("Custom Exception")
